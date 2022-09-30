abbdefd380ed1f4fc9413bc210aefa96e8194cdd
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-09-29 19:49:53 -0700

    getting random cards, working on random deck

--- game.js
+++ game.js
@@ -1,2 +1,15 @@
-const suits = ("clubs", "spades", "hearts", "diamonds");
-const cards = ( "ace", 2, 3, 4, 5, 6, 7, 8, 9, "jack", "queen", "king");
+const suits = ["clubs", "spades", "hearts", "diamonds"];
+const cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, "jack", "queen", "king"];
+
+const randomSuit = suits[Math.floor(Math.random() * 4)];
+const randomCards = cards[Math.floor(Math.random() * 12)];
+console.log(randomCards  + " of " + randomSuit);
+
+function createDeck() {
+    let deck = [];
+    while (deck.length < 52) {
+        deck.push(randomCards  + " of " + randomSuit);
+    }
+    return deck
+}
+console.log(createDeck())
