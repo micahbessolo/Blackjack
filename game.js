495424e0324c241e5ed8ee6020338b3606a1627e
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-10-01 08:02:02 -0700

    made a shuffle deck function

--- game.js
+++ game.js
@@ -1,14 +1,32 @@
+const cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 const suits = ["clubs", "spades", "hearts", "diamonds"];
-const cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, "jack", "queen", "king"];
 
-function createDeck() {
+function straightDeck() {
     let deck = [];
-    while (deck.length < 12) {
-        let card = cards[Math.floor(Math.random() * 12)]  + " of " + suits[Math.floor(Math.random() * 4)]
-        if (!deck.includes(card)){
-            deck.push(card)
+    for (let c = 0; c < cards.length; c++) {
+        for (let s = 0; s < suits.length; s++) {
+            deck.push(cards[c] + " of " + suits[s])
         }
     }
     return deck
 }
-console.log(createDeck())
+
+let deck = straightDeck();
+
+function shuffleDeck() {
+    let shuffledDeck = [];
+    let sortedDeck = deck;
+    while (sortedDeck.length > 0) {
+        // gets random position within the sorted deck
+        let randomCard = sortedDeck[Math.floor(Math.random() * sortedDeck.length)];
+        shuffledDeck.push(randomCard);
+        // removing the random card from the sorted deck
+        sortedDeck = sortedDeck.filter(function(item) {
+            return item !== randomCard;
+        })
+    }
+    return shuffledDeck
+}
+
+let deck = shuffleDeck()
+console.log(deck)
