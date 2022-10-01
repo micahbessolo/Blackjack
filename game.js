ecb303e7a022a47e72b85ecbbc5a61b08b5ff6d2
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-09-30 20:35:26 -0700

    working on deck shuffle

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
 const suits = ["clubs", "spades", "hearts", "diamonds"];
 const cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, "jack", "queen", "king"];
 
-const randomSuit = suits[Math.floor(Math.random() * 4)];
-const randomCards = cards[Math.floor(Math.random() * 12)];
-console.log(randomCards  + " of " + randomSuit);
-
 function createDeck() {
     let deck = [];
-    while (deck.length < 52) {
-        deck.push(randomCards  + " of " + randomSuit);
+    while (deck.length < 12) {
+        let card = cards[Math.floor(Math.random() * 12)]  + " of " + suits[Math.floor(Math.random() * 4)]
+        if (!deck.includes(card)){
+            deck.push(card)
+        }
     }
     return deck
 }
