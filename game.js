e66736f2834027c0206783b059aca655ced6a7de
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-10-01 12:28:39 -0700

    added hand scoring and improved frontend

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-const cards = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-const suits = ["clubs", "spades", "hearts", "diamonds"];
+const cards = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
+const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
 
 function straightDeck() {
     let deck = [];
     for (let c = 0; c < cards.length; c++) {
         for (let s = 0; s < suits.length; s++) {
-            deck.push(cards[c] + " of " + suits[s])
+            deck.push(cards[c] + ' of ' + suits[s])
         }
     }
     return deck
@@ -23,10 +23,40 @@ function shuffleDeck() {
         // removing the random card from the sorted deck
         sortedDeck = sortedDeck.filter(function(item) {
             return item !== randomCard;
-        })
+        });
     }
     return shuffledDeck
 }
 
-let deck = shuffleDeck()
-console.log(deck)
+let shuffledDeck = shuffleDeck()
+
+// picks the dealer's cards
+function dealerCards() {
+    dealer = [];
+    dealer.push(shuffledDeck[0], shuffledDeck[1]);
+    return dealer
+}
+
+console.log((dealerCards()));
+
+// give the current score of a hand
+function handValue(array) {
+    let score = 0;
+    // goes through cards
+    for (let i = 0; i < array.length; i++) {
+        // gets card rank (i.e ace, 4, jack)
+        const rank = array[i].split(" ")[0];
+        console.log(rank)
+        if (rank === 'jack' || rank === 'queen' || rank === 'king') {
+            score += 10;
+        }
+        else if (rank === 'ace') {
+            score += 11;
+        }
+        else {
+            score += Number(rank)
+        }
+    }
+    return score
+}
+console.log(handValue(dealerCards()));
